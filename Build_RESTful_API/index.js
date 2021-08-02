faf6d3fa9b3162ff8f85bb5df0aae4b715c16125
jmndao <jmndao@gmail.com>
2021-08-02 12:25:30 +0000

    Parsing Request Path

--- Build_RESTful_API/index.js
+++ Build_RESTful_API/index.js
@@ -4,10 +4,24 @@
 
 // Depedencies
 const http = require('http');
+const url = require('url');
 
 // The server should respond to all request with a string
 const server = http.createServer((req, res) => {
+
+    const baseURL = 'http://' + req.headers.host + '/';
+    // Get the URL and parse it
+    const parsedURL = new url.URL(req.url, baseURL);
+
+    // Get the path from that URL
+    const path = parsedURL.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Send the response
     res.end('Hello World\n');
+
+    // Log the request path
+    console.log('Request is received on path: ' + trimmedPath);
 })
 
 // Start the server and have it listen on port 3000
